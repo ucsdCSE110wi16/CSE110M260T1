efc6f970409e6aefad7ab1d44a328b675cb103e1
Yashwanth <Yashwanth@Yashwanths-MacBook-Pro.local>
2016-01-24 22:55:12 -0800

    Initial Commit with game skeleton

---
+++ android/src/com/cs110/app/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.cs110.app;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.cs110.app.CS110App;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new CS110App(), config);
+	}
+}

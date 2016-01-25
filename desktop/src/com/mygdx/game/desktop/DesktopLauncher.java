2c56882e0c096eb5caa9baa6f1f1244deb0caa5d
Marco Flowers <marco.flowers12@gmail.com>
2016-01-25 01:54:45 -0800

    Added desktop display
    The desktop gui will allow us to test the game without having to run
    an android emulator every time.

---
+++ desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.cs110.app.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.cs110.app.CS110App;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new CS110App(), config);
+	}
+}

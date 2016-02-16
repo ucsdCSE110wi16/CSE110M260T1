691c7089d45588392cac131fcf3f97aef903ed9a
OriGilad <ogilad@ucsd.edu>
2016-02-15 16:41:42 -0800

    Removed unused imports

--- core/src/com/cs110/app/CS110App.java
+++ core/src/com/cs110/app/CS110App.java
@@ -1,15 +1,12 @@
 
 package com.cs110.app;
 import com.badlogic.gdx.Game;
-import com.badlogic.gdx.ApplicationAdapter;
-import com.badlogic.gdx.Game;
-import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.GL20;
+
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.cs110.app.Screens.GameScreen;
 
-class CS110App extends Game { //The automatically generated code has ApplicationAdapter, but
+public class CS110App extends Game { //The automatically generated code has ApplicationAdapter, but
                                     // game allows for screens
 	SpriteBatch batch;
 	Texture img;

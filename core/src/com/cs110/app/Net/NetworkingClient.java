307e3a3de8f67fa304511ab5df930dd161ce8fc4
Yashwanth Nannapaneni <yashwanth620@gmail.com>
2016-02-18 22:08:48 -0800

    Small chnages

--- core/src/com/cs110/app/Net/NetworkingClient.java
+++ core/src/com/cs110/app/Net/NetworkingClient.java
@@ -12,7 +12,7 @@ import com.esotericsoftware.kryonet.Listener;
  */
 public class NetworkingClient extends Listener {
     static Client client;
-    static String ip="localhost";//"128.54.240.57";
+    static String ip="137.110.92.194";//"localhost";//"128.54.240.57";
     static int tcpPort = 27961, udpPort = 27961;
     float oldXCord, oldYCord;
 
